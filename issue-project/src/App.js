0f0e7fc185a93babf18167240f2ebaae341b26c1
zivivle <zivivle@gmail.com>
2023-06-15 22:39:06 +0900

    feat: 페이지네이션, 필터, 뒤로가기 기능 일부 구현

--- issue-project/src/App.js
+++ issue-project/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routing";
+import { worker } from "./apis/handler";
 
 function App() {
+  if (process.env.NODE_ENV === "development") {
+    worker.start();
+  }
   return <RouterProvider router={router} />;
 }
 
